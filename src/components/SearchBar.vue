9ece99de53e6bb358fd272c8d28239e97eb60814
Jordan Harris <64775152+jordanharris-6@users.noreply.github.com>
2024-11-08 16:39:16 +0000

    Change folder structure

---
+++ src/components/SearchBar.vue
@@ -0,0 +1,25 @@
+<template>
+  <input type="text" v-model="searchTerm" @input="onSearch" placeholder="Search..." />
+</template>
+
+<script setup>
+import { ref } from 'vue'
+import { useProductStore } from '@/stores/productStore'
+import debounce from 'lodash.debounce'
+
+const dataStore = useProductStore()
+const searchTerm = ref('')
+
+const onSearch = debounce(() => {
+  dataStore.setQuery(searchTerm.value)
+}, 300)
+</script>
+
+<style scoped>
+input {
+  padding: 20px;
+  border: 1px solid #d3d3d3;
+  border-radius: 10px;
+  font-weight: bold;
+}
+</style>
