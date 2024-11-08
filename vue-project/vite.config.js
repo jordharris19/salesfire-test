0e641ca1e2b795761c0a5db4dfe2e8f05b6bab23
Jordan Harris <64775152+jordanharris-6@users.noreply.github.com>
2024-11-08 16:56:37 +0000

    correct api address

--- vue-project/vite.config.js
+++ vue-project/vite.config.js
@@ -3,15 +3,6 @@ import vue from '@vitejs/plugin-vue'
 import { fileURLToPath, URL } from 'url'
 
 export default defineConfig({
-  server: {
-    proxy: {
-      '/api': {
-        target: 'https://aix.salesfire.co.uk',
-        changeOrigin: true,
-        rewrite: (path) => path,
-      },
-    },
-  },
   plugins: [vue()],
   resolve: {
     alias: {
